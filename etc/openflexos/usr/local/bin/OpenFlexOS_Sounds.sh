d3b08c1fba0a2f9586f2f10ca929f361cf99dca3
Chris K Lee <chrislee2511@gmail.com>
2025-10-26 20:13:36 +0000

    changed openflexos sounds script with new sounds

--- etc/openflexos/usr/local/bin/OpenFlexOS_Sounds.sh
+++ etc/openflexos/usr/local/bin/OpenFlexOS_Sounds.sh
@@ -28,10 +28,10 @@ elif [[ "$WM" == "openbox" ]]; then
 fi
 
 # Sound file definitions
-login_sound="game-bonus-144751.mp3"
-lock_sound="ambient-piano-logo-165357.mp3"
-logout_sound="marimba-win-f-2-209688.mp3"
-reboot_sound="introduction-sound-201413.mp3"
-suspend_sound="cozy-weaves-soft-logo-176378.mp3"
-hibernate_sound="lovelyboot1-103697.mp3"
-poweroff_sound="retro-audio-logo-94648.mp3"
+login_sound="elegant-hitech-logo-165371.mp3"
+lock_sound="hitech-logo-165392.mp3"
+logout_sound="sport-theme-tv-logo-154233.mp3"
+reboot_sound="radio-logo-154127.mp3"
+suspend_sound="computer-startup-music-97699.mp3"
+hibernate_sound="startup-sound-variation-6-316850.mp3"
+poweroff_sound="sanyo-scp-device-shutdown-320506.mp3"
